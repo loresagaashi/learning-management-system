b1cac29bdf5ebfc393149e47511b64305d250d25
Adea Piperku <ap64975@ubt-uni.net>
2025-03-18 00:49:20 +0100

    Model for Assignment, Grade and Submission

---
+++ learning-management-system/src/main/java/com/learning_management_system/model/Grade.java
@@ -0,0 +1,25 @@
+package com.learning_management_system.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+public class Grade extends BaseEntity {
+    
+   @ManyToOne
+   @JoinColumn(name = "assignment_id", nullable = true, foreignKey = @ForeignKey(name = "fk_grade_assignment", foreignKeyDefinition = "FOREIGN KEY (assignment_id) REFERENCES Assignment(id) ON DELETE RESTRICT"))
+   private Assignment assignment;
+    
+   // @ManyToOne
+   // @JoinColumn(name = "student_id", nullable = true, foreignKey = @ForeignKey(name = "fk_grade_student", foreignKeyDefinition = "FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE RESTRICT"))
+   // private Student student;
+    
+   private Double grade;
+
+}
