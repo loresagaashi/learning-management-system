e419e4406d7c797f88e234a4dac23bfe5b6f2e18
Loresa <loresag17@gmail.com>
2025-03-19 00:20:51 +0100

    Material Repository,Service,Controller

---
+++ learning-management-system/src/main/java/com/learning_management_system/service/MaterialService.java
@@ -0,0 +1,14 @@
+package com.learning_management_system.service;
+
+import org.springframework.stereotype.Service;
+
+import com.learning_management_system.model.Material;
+import com.learning_management_system.repository.MaterialRepository;
+
+@Service
+public class MaterialService extends BasicServiceOperations<MaterialRepository, Material>{
+
+    public MaterialService(MaterialRepository repository){
+        super(repository);
+    }
+}
