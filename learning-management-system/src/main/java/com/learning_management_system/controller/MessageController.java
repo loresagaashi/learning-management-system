379dc3f5bea6ee55c6a54803ecf27c839c88aaeb
Olti Berbatovci <oltiberbatovci76@gmail.com>
2025-03-19 02:44:09 +0100

    Message Repository, Service and Controller

---
+++ learning-management-system/src/main/java/com/learning_management_system/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.learning_management_system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning_management_system.model.Message;
+import com.learning_management_system.service.MessageService;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController extends BasicControllerOperations<MessageService,Message>{
+
+    public MessageController(MessageService service){
+        super(service);
+    }
+
+}
