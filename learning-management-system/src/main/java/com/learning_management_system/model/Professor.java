9999b6451ca2f1463d2a2b99b22837dd5218fa3b
Benina <benina.sharapolli@gmail.com>
2025-03-23 00:03:58 +0100

    UserAccount, Admin, Student Professor models

---
+++ learning-management-system/src/main/java/com/learning_management_system/model/Professor.java
@@ -0,0 +1,13 @@
+package com.learning_management_system.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+public class Professor extends UserAccount {
+    private String fullName;
+    private String department;
+}
