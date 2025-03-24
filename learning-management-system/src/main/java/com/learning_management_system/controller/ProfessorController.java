3e6502427b70cef000d7c7d5892b7e9d6cfe89ee
Loresa <loresag17@gmail.com>
2025-03-25 00:23:22 +0100

    JWT and Tokens

--- learning-management-system/src/main/java/com/learning_management_system/controller/ProfessorController.java
+++ learning-management-system/src/main/java/com/learning_management_system/controller/ProfessorController.java
@@ -1,8 +1,12 @@
 package com.learning_management_system.controller;
 
 import com.learning_management_system.model.Professor;
+import com.learning_management_system.payload.LoginPayload;
 import com.learning_management_system.service.ProfessorService;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,10 @@ public class ProfessorController extends BasicControllerOperations<ProfessorServ
 
     public ProfessorController(ProfessorService service){
         super(service);
+    }
+
+        @PostMapping("/login")
+    public Professor login(@RequestBody @Validated LoginPayload login) {
+        return this.service.login(login.getEmail(), login.getPassword());
     }
 }
--- learning-management-system/src/main/java/com/learning_management_system/service/AdminService.java
+++ learning-management-system/src/main/java/com/learning_management_system/service/AdminService.java
@@ -1,14 +1,54 @@
 package com.learning_management_system.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
+import com.learning_management_system.exception.EntityValidationException;
+import com.learning_management_system.exception.ExceptionPayload;
 import com.learning_management_system.model.Admin;
 import com.learning_management_system.repository.AdminRepository;
 
 @Service
 public class AdminService extends BasicServiceOperations<AdminRepository, Admin> {
 
-   public AdminService(AdminRepository repository) {
-      super(repository);
-   }
+   private final PasswordEncoder passwordEncoder;
+
+    public AdminService(AdminRepository repository, PasswordEncoder passwordEncoder) {
+        super(repository);
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Optional<Admin> findByEmail(String email) {
+        return this.repository.findByEmail(email);
+    }
+
+    @Override
+    public Admin save(Admin entity) {
+        if (entity.getId() == null) {
+            entity.setPassword(passwordEncoder.encode(entity.getPassword()));
+        }
+
+        return super.save(entity);
+    }
+
+    public Admin login(String email, String password) {
+        Admin admin = this.repository.findByEmail(email)
+                .orElseThrow(() -> new EntityValidationException(ExceptionPayload.builder()
+                        .code("WrongEmail")
+                        .fieldName("email")
+                        .rejectedValue(email)
+                        .message("Wrong email")
+                        .build())
+                );
+        if (!admin.getPassword().equals(password)) {
+            throw new EntityValidationException(ExceptionPayload.builder()
+                    .code("WrongPassword")
+                    .fieldName("password")
+                    .rejectedValue(password)
+                    .message("Wrong password")
+                    .build());
+        }
+
+        return admin;
+    }
 }
