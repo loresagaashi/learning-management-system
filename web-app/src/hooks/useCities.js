c888c812e6a03e661195ae47de283f8577d56a6e
Adea Piperku <ap64975@ubt-uni.net>
2025-03-27 02:21:19 +0100

    MuiAppBar, useCities and UserContext

---
+++ web-app/src/hooks/useCities.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { axiosInstance } from '../service/axiosInstance';
+
+const useCities = () => {
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        const response = await axiosInstance.get('/cities/all');
+        setCities(response);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchCities();
+  }, []);
+
+  return { cities, loading, error };
+};
+
+export default useCities;
