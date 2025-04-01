627930b97f22cfbea4f671c9114acca3032a60f2
RilindS <rilindsimnica09@gmail.com>
2025-04-01 19:29:41 +0200

    email service and controller

---
+++ learning-management-system/src/main/java/com/learning_management_system/controller/EmailController.java
@@ -0,0 +1,23 @@
+package com.learning_management_system.controller;
+
+import com.learning_management_system.data.email.SendEmailDTO;
+import com.learning_management_system.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/email")
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/sendEmail")
+    public String sendEmail(@RequestBody SendEmailDTO sendEmailDTO) {
+        emailService.sendSimpleEmail(sendEmailDTO);
+        return "Email sent successfully!";
+    }
+}
