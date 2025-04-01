bc35971b40553124ababd6f96736a67f58c1615f
Loresa <loresag17@gmail.com>
2025-04-01 15:19:54 +0200

    All CRUD operations for Lecture in front-end

---
+++ web-app/src/page/admin/lectures/LecturesView.js
@@ -0,0 +1,76 @@
+import CustomMaterialTable from "../../../component/dashboard/CustomMaterialTable";
+import { useRef } from "react";
+import {
+  SelectTableCell,
+  TextFieldTableCell,
+} from "../../../component/TableCells";
+import { QueryKeys } from "../../../service/QueryKeys";
+import DateFnsUtils from "@date-io/date-fns";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import LockIcon from "@material-ui/icons/Lock";
+import { StudentService } from "../../../service/StudentService";
+import { CourseService } from "../../../service/CourseService";
+import { LectureService } from "../../../service/LectureService";
+import { useQuery } from "react-query";
+
+const lectureService = new LectureService();
+
+const courseService = new CourseService();
+
+export default function LecturesView({}) {
+  const errorRef = useRef();
+
+  const { data: allCourses } = useQuery(QueryKeys.COURSE, () =>
+    courseService.findAll()
+  );
+  const columns = [
+    {
+      title: "Id",
+      field: "id",
+      editComponent: (props) => TextFieldTableCell(props, errorRef),
+    },
+    {
+      title: "Course",
+      field: "course",
+      render: (rowData) => rowData.course?.name,
+      editComponent: (props) =>
+        SelectTableCell(
+          props,
+          errorRef,
+          allCourses?.map((x) => ({ value: x, label: x.name })) || [],
+          "id"
+        ),
+    },
+    {
+      title: "Lecture Date",
+      type: "date",
+      field: "lectureDate",
+      editComponent: (props) => (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DatePicker
+            value={props.value}
+            onChange={(date) => props.onChange(date)}
+            format="yyyy-MM-dd"
+            inputVariant="outlined"
+            fullWidth
+          />
+        </MuiPickersUtilsProvider>
+      ),
+    },
+    {
+      title: "Topic",
+      field: "topic",
+      editComponent: (props) => TextFieldTableCell(props, errorRef),
+    },
+  ];
+
+  return (
+    <CustomMaterialTable
+      title="Manage Courses"
+      columns={columns}
+      service={courseService}
+      queryKey={QueryKeys.COURSE}
+      errorRef={errorRef}
+    />
+  );
+}
