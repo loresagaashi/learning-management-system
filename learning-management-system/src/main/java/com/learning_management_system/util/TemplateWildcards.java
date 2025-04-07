5dc6ce8d5f29ebf6c9ddd69ab390ee111947a2e5
RilindS <rilindsimnica09@gmail.com>
2025-04-07 17:28:18 +0200

    replace wildCards method

--- learning-management-system/src/main/java/com/learning_management_system/util/TemplateWildcards.java
+++ learning-management-system/src/main/java/com/learning_management_system/util/TemplateWildcards.java
@@ -32,6 +32,13 @@ public abstract class TemplateWildcards {
     public final static String PROFESSOR_PHONE_NUMBER = "professorPhoneNumber";
     public final static String PROFESSOR_BIRTH_DATE = "professorBirthDate";
 
+    public final static String USER_FIRST_NAME = "userFirstName";
+    public final static String USER_LAST_NAME = "userLastName";
+
+    public final static String USER_EMAIL = "userEmail";
+    public final static String USER_ID = "userId";
+    public static final String REQUEST_TIME = "#requestTime#";
+    public static final String TOKEN = "#token#";
 
     //TODO add other
 
