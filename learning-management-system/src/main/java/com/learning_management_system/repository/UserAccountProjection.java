7811b5d63c885f23ff4b562f4cf64352fe857731
GJKK <rilindsimnica09@gmail.com>
2025-04-29 19:01:42 +0200

    added userId in /me

--- learning-management-system/src/main/java/com/learning_management_system/repository/UserAccountProjection.java
+++ learning-management-system/src/main/java/com/learning_management_system/repository/UserAccountProjection.java
@@ -3,6 +3,7 @@ package com.learning_management_system.repository;
 import java.time.LocalDate;
 
 public interface UserAccountProjection {
+    Long getUserId();
     String getFirstName();
     String getLastName();
     String getEmail();
