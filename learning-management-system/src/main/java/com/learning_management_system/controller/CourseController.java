e0f4a7011a1baa600659958a02cf12dbb7a4bfd1
RilindS <rilindsimnica09@gmailcom>
2025-05-22 00:02:29 +0200

    endpoint for get unpassed exam

--- learning-management-system/src/main/java/com/learning_management_system/controller/CourseController.java
+++ learning-management-system/src/main/java/com/learning_management_system/controller/CourseController.java
@@ -1,7 +1,9 @@
 package com.learning_management_system.controller;
 
+import com.learning_management_system.data.course.CourseDTO;
 import com.learning_management_system.model.Lecture;
 import com.learning_management_system.model.Student;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,12 @@ public class CourseController extends BasicControllerOperations<CourseService,Co
     @GetMapping("/{courseId}/lectures")
     public List<Lecture> getLecturesByCourse(@PathVariable Long courseId) {
         return service.getLecturesByCourse(courseId);
+    }
+
+    @GetMapping("/unpassed/{studentId}")
+    public ResponseEntity<List<CourseDTO>> getUnpassedCourses(@PathVariable Long studentId) {
+        List<CourseDTO> result = service.getUnpassedCourses(studentId);
+        return ResponseEntity.ok(result);
     }
     
 }
--- learning-management-system/src/main/java/com/learning_management_system/repository/CourseRepository.java
+++ learning-management-system/src/main/java/com/learning_management_system/repository/CourseRepository.java
@@ -2,12 +2,42 @@ package com.learning_management_system.repository;
 
 // import java.util.Optional;
 
+import com.learning_management_system.data.course.CourseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learning_management_system.model.Course;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long>{
     List<Course> findByProfessorId(Long professorId);
+
+    @Query("""
+                SELECT new com.learning_management_system.data.course.CourseDTO(
+                    c.id,
+                    c.name,
+                    c.description,
+                    e.id,
+                    e.professor.firstName
+                )
+                FROM Schedule sc
+                JOIN sc.groupSemester gs
+                JOIN gs.group g
+                JOIN Student s ON s.group = g
+                JOIN sc.course c
+                JOIN Exam e ON e.course = c
+                WHERE s.id = :studentId
+                  AND (
+                    NOT EXISTS (
+                      SELECT ea FROM ExamApplication ea
+                      WHERE ea.exam.course = c
+                        AND ea.student.id = :studentId
+                        AND ea.status = 'PASSED'
+                    )
+                  )
+            """)
+    List<CourseDTO> findUnpassedOrUnattemptedCoursesByStudentId(@Param("studentId") Long studentId);
+
 }
