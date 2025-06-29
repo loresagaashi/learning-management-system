2aefc2b7ba6161dd316f713abd69e33a22aa571d
loresagaashi <loresag17@gmail.com>
2025-06-29 02:03:20 +0200

    assignment in courses for professor and student

--- learning-management-system/src/main/java/com/learning_management_system/service/AssignmentService.java
+++ learning-management-system/src/main/java/com/learning_management_system/service/AssignmentService.java
@@ -5,10 +5,16 @@ import org.springframework.stereotype.Service;
 import com.learning_management_system.model.Assignment;
 import com.learning_management_system.repository.AssignmentRepository;
 
+import java.util.List;
+
 @Service
 public class AssignmentService extends BasicServiceOperations<AssignmentRepository, Assignment> {
 
    public AssignmentService(AssignmentRepository repository) {
       super(repository);
+   }
+
+   public List<Assignment> findByCourseId(Long courseId) {
+      return repository.findByCourseId(courseId);
    }
 }
